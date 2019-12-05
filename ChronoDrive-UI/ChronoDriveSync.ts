955abc40a999509092398ceab0be2060e5f4f8f8
Seth Redwine <sredwine@unomaha.edu>
2019-12-05 00:51:54 -0600

    less specific prefix

--- ChronoDrive-UI/ChronoDriveSync.ts
+++ ChronoDrive-UI/ChronoDriveSync.ts
@@ -43,7 +43,7 @@ const ChronoDriveSync = function (userName: string, fileInfo: FileInfo, userDirC
   this.certificateName = certificateName;
 
   // TODO: rename prefix - we're filing, not chatting
-  this.sync_prefix = (new Name(hubPrefix)).append(this.userName).append(this.getRandomString());
+  this.sync_prefix = (new Name(hubPrefix)).append(this.userName); //.append(this.getRandomString());
   console.log('Prefix: ', this.sync_prefix.toUri());
 
   // QUESTION: Do we continue to use this roster? this will make it possible to keep stored users' directories updated without relying on 
